3ba4405a0e64332f9ae81b160e5b63fa75fd1b5b
ultimate010 <ultimate010@gmail.com>
2013-09-08 12:53:50 +0800

    Just for sync

--- preprocessing/preDict.pl
+++ preprocessing/preDict.pl
@@ -1,17 +1,23 @@
 #!/usr/bin/perl
-#删掉文本中的符号，将所有间隔符换为一个空格
+#文本替换成id并统计信息
 #Zaks Wang
-#2013-5-13
+#2013-9-8
 use Encode;
 use utf8;
+open(outDict,">word2Id.txt");
+open(outWordCount,">wordCount.txt");
+open(outFile,">text.txt");
+
 $wordTotal = 0;
 $wordCount = 0;
+$lineCount = 0;
 while($line = <stdin>){
   #print $line;
   chomp($line);
   if(length($line) == 0){
     next;
   }
+  ++$lineCount;
   @arr = split(/[ \t]/,$line);
   $first = 0;
   foreach(@arr){
@@ -23,22 +29,25 @@ while($line = <stdin>){
         $wordCount++;
       }
       if($first != 0){
-        print "\t";
+        print outFile "\t";
       }else{
         $first = 1;
       }
-      print "$hashWordToId{$_}";
+      print outFile encode("utf8","$hashWordToId{$_}");
       $hashCount{$_}++;
     }else{
     }
   }
-  print "\n";
+  print outFile "\n";
 }
-foreach(sort keys %hashWordToId){
-  print "Word2Id: key:$_ value:$hashWordToId{$_}\n";
-}
-
-foreach(sort keys %hashCount){
-  print "WordCount: key:$_ value:$hashCount{$_}\n";
+print outFile encode("utf8","TotalLine:$lineCount\tDifWordCount:$wordCount\tTotalWords:$wordTotal\n");
+foreach(sort { $hashWordToId{$a} <=> $hashWordToId{$b} } keys %hashWordToId){
+  print outDict encode("utf8","Word2Id: key:$_\tvalue:$hashWordToId{$_}\n");
 }
 
+foreach(sort{ $hashCount{$a} <=> $hashCount{$b} } keys %hashCount){
+  print outWordCount  encode("utf8","WordCount: key:$_\tvalue:$hashCount{$_}\n");
+}
+close(outDict);
+close(outWordCount);
+close(outFile);
