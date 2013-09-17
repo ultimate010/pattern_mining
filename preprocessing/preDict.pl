664137aaa8442eff3eed7ae3d60f6da9f4c57512
ultimate010 <ultimate010@gmail.com>
2013-09-17 17:30:09 +0800

    Word2Id

--- preprocessing/preDict.pl
+++ preprocessing/preDict.pl
@@ -4,16 +4,20 @@
 #2013-9-8
 use Encode;
 use utf8;
+open(in,"test.file");
 open(outDict,">word2Id.txt");
+open(outZhDict,">zhWord2Id.txt");
 open(outWordCount,">wordCount.txt");
 open(outFile,">text.txt");
 
 $wordTotal = 0;
 $wordCount = 0;
 $lineCount = 0;
-while($line = <stdin>){
+$flagZh = 1;
+while($line = <in>){
   #print $line;
   chomp($line);
+  $line = decode("utf8",$line);
   if(length($line) == 0){
     next;
   }
@@ -26,6 +30,9 @@ while($line = <stdin>){
       if(defined $hashCount{$_}){
       }else{
         $hashWordToId{$_} = $wordCount;
+        if($flagZh == 1){
+          $hashZhToId{$_} = $wordCount;
+        }
         $wordCount++;
       }
       if($first != 0){
@@ -38,16 +45,25 @@ while($line = <stdin>){
     }else{
     }
   }
+  if($flagZh == 1){
+    $flagZh = 0;
+  }else{
+    $flagZh = 1;
+  }
   print outFile "\n";
 }
 print outFile encode("utf8","TotalLine:$lineCount\tDifWordCount:$wordCount\tTotalWords:$wordTotal\n");
 foreach(sort { $hashWordToId{$a} <=> $hashWordToId{$b} } keys %hashWordToId){
-  print outDict encode("utf8","Word2Id: key:$_\tvalue:$hashWordToId{$_}\n");
+  print outDict encode("utf8","$_\t$hashWordToId{$_}\n");
 }
-
+foreach(sort { $hashZhToId{$a} <=> $hashZhToId{$b} } keys %hashZhToId){
+  print outZhDict encode("utf8","$_\t$hashZhToId{$_}\n");
+}
 foreach(sort{ $hashCount{$a} <=> $hashCount{$b} } keys %hashCount){
-  print outWordCount  encode("utf8","WordCount: key:$_\tvalue:$hashCount{$_}\n");
+  print outWordCount  encode("utf8","$_\t$hashCount{$_}\n");
 }
+close(in);
 close(outDict);
+close(outZhDict);
 close(outWordCount);
 close(outFile);
