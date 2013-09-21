7e5317bd251ce41e179746b9cd778082e442ffa1
ultimate010 <ultimate010@gmail.com>
2013-09-21 21:21:40 +0800

    修改preDict.pl使能处理一行中文一行英文数据

--- preprocessing/preDict.pl
+++ preprocessing/preDict.pl
@@ -4,7 +4,7 @@
 #2013-9-8
 use Encode;
 use utf8;
-open(in,"test.file");
+open(in,"$ARGV[0]");
 open(outDict,">word2Id.txt");
 open(outZhDict,">zhWord2Id.txt");
 open(outWordCount,">wordCount.txt");
@@ -21,9 +21,7 @@ while($line = <in>){
   if(length($line) == 0){
     next;
   }
-  ++$lineCount;
   @arr = split(/[ \t]/,$line);
-  $first = 0;
   foreach(@arr){
     if(length($_) >= 1){
       $wordTotal++;
@@ -35,24 +33,46 @@ while($line = <in>){
         }
         $wordCount++;
       }
+      $hashCount{$_}++;
+    }else{
+    }
+  }
+  if($flagZh == 1){
+    ++$lineCount;
+    $flagZh = 0;
+  }else{
+    $flagZh = 1;
+  }
+}
+close(in);
+print outFile encode("utf8","$lineCount\t$wordCount\t$wordTotal\n");
+open(in,"$ARGV[0]");
+while($line = <in>){
+  chomp($line);
+  $line = decode("utf8",$line);
+  $line2 = <in>; chomp($line2);
+  $line2 = decode("utf8",$line2);
+  if(length($line) == 0 || length($line2) == 0){
+    print "Wrong\n";
+    next;
+  }
+  $line = $line."\t".$line2;
+  @arr = split(/[ \t]/,$line);
+  $first = 0;
+  foreach(@arr){
+    if(length($_) >= 1){
       if($first != 0){
         print outFile "\t";
       }else{
         $first = 1;
       }
       print outFile encode("utf8","$hashWordToId{$_}");
-      $hashCount{$_}++;
     }else{
     }
   }
-  if($flagZh == 1){
-    $flagZh = 0;
-  }else{
-    $flagZh = 1;
-  }
   print outFile "\n";
 }
-print outFile encode("utf8","TotalLine:$lineCount\tDifWordCount:$wordCount\tTotalWords:$wordTotal\n");
+close(in);
 foreach(sort { $hashWordToId{$a} <=> $hashWordToId{$b} } keys %hashWordToId){
   print outDict encode("utf8","$_\t$hashWordToId{$_}\n");
 }
@@ -62,7 +82,6 @@ foreach(sort { $hashZhToId{$a} <=> $hashZhToId{$b} } keys %hashZhToId){
 foreach(sort{ $hashCount{$a} <=> $hashCount{$b} } keys %hashCount){
   print outWordCount  encode("utf8","$_\t$hashCount{$_}\n");
 }
-close(in);
 close(outDict);
 close(outZhDict);
 close(outWordCount);
